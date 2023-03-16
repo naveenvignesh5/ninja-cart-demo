078cf186ca42916b64852df23673576cf59a68f8
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2023-03-16 16:42:47 +0530

    feat: add searchable dropdown with hightlight

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([]);
+
+  // NOTE: update your test data here
+  const fruitList = useMemo(
+    () => [
+      "apple",
+      "orange",
+      "pineapple",
+      "strawberry",
+      "mango",
+      "berry blue berry",
+      "apple pine and apple",
+    ],
+    []
+  );
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+
+    if (!e.target.value) {
+      setList([]);
+      return;
+    }
+
+    setList(fruitList.filter((o) => o.indexOf(value) > -1));
+  };
+
+  const handleItemClick = (o) => {
+    setValue(o);
+    setList([]);
+  };
+
+  const renderListItem = (o) => {
+    const parts = o.split(new RegExp(`(${value})`, "gi"));
+
+    return (
+      <span>
+        {parts.map((p, i) => (
+          <span
+            key={`${p}-${i}`}
+            className={
+              p.toLowerCase() === value.toLowerCase() ? "highlight" : ""
+            }
+          >
+            {p}
+          </span>
+        ))}
+      </span>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h4 className="app__title">
+        Ninjacart - Interview - Round 1 - Assignment
+      </h4>
+      <div className="form-group">
+        <label>Search fruit</label>
+        <div className="dropdown">
+          <input type="text" value={value} onChange={handleChange} />
+          {!!list.length && (
+            <div className="list">
+              {list.map((o) => (
+                <button
+                  key={o}
+                  className="list-item"
+                  onClick={() => handleItemClick(o)}
+                >
+                  {renderListItem(o)}
+                </button>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
